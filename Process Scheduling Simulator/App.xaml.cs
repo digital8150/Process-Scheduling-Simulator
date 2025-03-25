a2597bcf458c03412fa543d31b31e52208d740b0
Digital <73296205+digital8150@users.noreply.github.com>
2025-03-25 22:00:02 +0900

    초기 커밋.

---
+++ Process Scheduling Simulator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Process_Scheduling_Simulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
