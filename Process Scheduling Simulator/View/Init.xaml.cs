8131ba35940625e82f989515f8351c6a7a12bf66
Digital <73296205+digital8150@users.noreply.github.com>
2025-03-26 16:22:33 +0900

    Chore: 프로젝트 구조 개선

---
+++ Process Scheduling Simulator/View/Init.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Process_Scheduling_Simulator.View
+{
+    /// <summary>
+    /// Init.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Init : Window
+    {
+        public Init()
+        {
+            InitializeComponent();
+        }
+    }
+}
